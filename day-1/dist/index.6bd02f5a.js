3880be960aebe94e5b708187016092499a7878e2
poorvaditya behre <aditya.behre@gmail.com>
2024-02-26 11:03:58 +0530

    added parcel and how to build for production

---
+++ day-1/dist/index.6bd02f5a.js
@@ -0,0 +1,23 @@
+const heading1 = React.createElement("h1", // providing attributes
+{
+    id: "heading1",
+    className: "heading"
+}, "Heading 1!");
+const heading2 = React.createElement("h1", // providing attributes
+{
+    id: "heading2",
+    className: "heading"
+}, "Heading 2!");
+const container = React.createElement("div", {
+    id: "container"
+}, // putting multiple childrens inside div in the form of array
+[
+    heading1,
+    heading2
+]);
+// we need to tell react what is the root element inside our app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside the root
+root.render(container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
