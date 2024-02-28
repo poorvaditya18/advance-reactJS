c740569f90aa573447198ab76ab5f8b20171d914
poorvaditya behre <aditya.behre@gmail.com>
2024-02-28 13:22:51 +0530

    added restaurant layout and setup structure

--- day-1/App.js
+++ day-1/App.js
@@ -1,26 +1,328 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// react element
-const title = (
-  <h1 className="Title" tabIndex="5">
-    Hey Everyone Title
-  </h1>
-);
+const Header = () => {
+  // header will have logo and navItem
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://assets.materialup.com/uploads/166d4a30-2f5a-4ef5-9634-9b828b42224d/preview.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          {/* further you can put cart image here */}
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// const data = api.getData()
+const RestaurantCard = (props) => {
+  // RestaurantCard will have -> img , name of restaurant , star rating , cuisine, delivery time.
+  const { resData } = props;
 
-// Functional Component
-const HeadingComponent = () => (
-  <div id="container">
-    {/* {data} */}
-    <h1>{console.log(100 + 200)}</h1>
-    <h1 className="heading">Namaste React Functional Component</h1>
-  </div>
-);
+  // good way to always destructure your data
+  const {
+    name,
+    cuisines,
+    avgRating,
+    cloudinaryImageId,
+    costForTwo,
+    sla: { deliveryTime },
+  } = resData?.info;
 
-// ReactDOM used for interacting with DOM elements
+  return (
+    <div className="restaurant-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        // Good practise : image coming from cdn
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      ></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+// dummy swiggy api data -->
+const resList = [
+  {
+    info: {
+      id: "14781",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Shankar Kalat Nagar",
+      areaName: "Wakad",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.8,
+      parentId: "721",
+      avgRatingString: "3.8",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-29 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c2b278a4-9e02-4021-9ec9-8f15cd2b7515",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-shankar-kalat-nagar-wakad-pune-14781",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "673425",
+      name: "Wow! Momo",
+      cloudinaryImageId: "64fd45fd9f44c1737bc446e470bed666",
+      locality: "GHS",
+      areaName: "RGI Park",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "Tibetan",
+        "Healthy Food",
+        "Asian",
+        "Chinese",
+        "Snacks",
+        "Continental",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 3.8,
+      parentId: "1776",
+      avgRatingString: "3.8",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 42,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-29 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c2b278a4-9e02-4021-9ec9-8f15cd2b7515",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/wow-momo-ghs-rgi-park-pune-673425",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "692121",
+      name: "BOOM Sandwich - Sub of India",
+      cloudinaryImageId: "96aaf72372ad1b0297b7cc304ced7e33",
+      locality: "Blue Ridge",
+      areaName: "Hinjawadi",
+      costForTwo: "₹250 for two",
+      cuisines: ["Snacks", "Indian", "Desserts"],
+      avgRating: 4.3,
+      parentId: "401169",
+      avgRatingString: "4.3",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 0.9,
+        serviceability: "SERVICEABLE",
+        slaString: "16-26 mins",
+        lastMileTravelString: "0.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-29 02:00:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹110",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-c2b278a4-9e02-4021-9ec9-8f15cd2b7515",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/boom-sandwich-sub-of-india-blue-ridge-hinjawadi-pune-692121",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-container"> Search</div>
+      <div className="restaurant-container">
+        {/* they will contain a lot of restaurant card. We will be reusing the restaurantCard component. This increases our code reusability.*/}
+
+        {/* NOTE : Not good practise */}
+        {/* <RestaurantCard resData={resList[0]} />
+        <RestaurantCard resData={resList[1]} />
+        <RestaurantCard resData={resList[2]} /> */}
+
+        {/* NOTE : instead of for loop to iterate over resList. use map(), filter(), reduce() */}
+        {resList.map((restaurant) => (
+          // <RestaurantCard/> is just a function and it is returing some jsx code. Pretty Simple and understandable.
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// App Layout
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+      <Body></Body>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// rendering functional component
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
