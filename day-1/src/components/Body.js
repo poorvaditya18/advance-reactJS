c697631657d0be0033adb0616eb60749e53288c6
poorvaditya behre <aditya.behre@gmail.com>
2024-02-29 10:42:19 +0530

    added how we can use useState hook

--- day-1/src/components/Body.js
+++ day-1/src/components/Body.js
@@ -1,21 +1,32 @@
 import RestaurantCard from "./RestaurantCard.js";
+import { useState } from "react";
 import resList from "../utils/mockData.js";
 
 // Body component
 const Body = () => {
+  // useState variable
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search-container"> Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Filter logic here
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            // Now we are updating the listOfRestaurant using state variable (useState())
+            setlistOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="restaurant-container">
-        {/* they will contain a lot of restaurant card. We will be reusing the restaurantCard component. This increases our code reusability.*/}
-
-        {/* NOTE : Not good practise */}
-        {/* <RestaurantCard resData={resList[0]} />
-          <RestaurantCard resData={resList[1]} />
-          <RestaurantCard resData={resList[2]} /> */}
-
         {/* NOTE : instead of for loop to iterate over resList. use map(), filter(), reduce() */}
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           // <RestaurantCard/> is just a function and it is returing some jsx code. Pretty Simple and understandable.
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
--- day-1/src/utils/mockData.js
+++ day-1/src/utils/mockData.js
@@ -229,6 +229,120 @@ const resList = [
     },
     widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
   },
+  {
+    info: {
+      id: "23716",
+      name: "McDonald's",
+      cloudinaryImageId: "03501c33ecb3a3105124441e541e6fe4",
+      locality: "Marunji Road",
+      areaName: "Hinjawadi",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      avgRating: 4.4,
+      parentId: "630",
+      avgRatingString: "4.4",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 0.7,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "0.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-01 03:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mcdonalds-marunji-road-hinjawadi-pune-23716",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "3417",
+      name: "Subway",
+      cloudinaryImageId: "63178e3e64d503a479f2a2048a474552",
+      locality: "Blue Ridge Township",
+      areaName: "Hinjawadi",
+      costForTwo: "₹350 for two",
+      cuisines: ["Healthy Food", "Salads", "Snacks", "Desserts", "Beverages"],
+      avgRating: 4.2,
+      parentId: "2",
+      avgRatingString: "4.2",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 38,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-29 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/subway-blue-ridge-township-hinjawadi-pune-3417",
+      type: "WEBLINK",
+    },
+  },
 ];
 
 export default resList;
