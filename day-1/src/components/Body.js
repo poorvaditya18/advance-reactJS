fef637ee35e8574611da454fea44d7f2312a23fc
poorvaditya behre <aditya.behre@gmail.com>
2024-03-01 18:42:32 +0530

    added search and top rated restaurant functionality

--- day-1/src/components/Body.js
+++ day-1/src/components/Body.js
@@ -6,6 +6,10 @@ import Shimmer from "./Shimmer.js";
 const Body = () => {
   // useState variable
   const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+  // making a copy of listOfRestaurants for search functionality
+  const [filterRestaurantList, setfilterRestaurantList] = useState([]);
+
+  const [searchText, setsearchText] = useState("");
 
   useEffect(() => {
     // callback function will be called after the component is rendered
@@ -26,6 +30,7 @@ const Body = () => {
         ?.restaurants;
     // now put this new data inside the listOfRestaurant
     setlistOfRestaurants(parsedDataList);
+    setfilterRestaurantList(parsedDataList);
   };
 
   return listOfRestaurants.length === 0 ? (
@@ -33,24 +38,51 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="searchBox"
+            value={searchText}
+            // we need to provide the onChange handler
+            // this will update the searchText state variable
+            // so, whenever the searchText is getting on every key press then whole body component is getting re-rendered.
+            onChange={(e) => {
+              // this onChange method is updating the searchText
+              // therefore react is triggering the reconcilation cycle
+              // whole body component is getting re-rendered but only updating the input text box due to diff algorithm. React is very efficient in checking the difference.
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // filter the restaurants card and update the UI
+              // search Text
+              console.log(searchText);
+              // now we can update the listOfRestaurants
+              const filterRestaurantBySearch = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText)
+              );
+              setfilterRestaurantList(filterRestaurantBySearch);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             // Filter logic here
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4
+            const filterTopRatedRestaurantList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
             );
-            // Now we are updating the listOfRestaurant using state variable (useState())
-            setlistOfRestaurants(filteredList);
+            setfilterRestaurantList(filterTopRatedRestaurantList);
           }}
         >
           Top Rated Restaurant
         </button>
       </div>
       <div className="restaurant-container">
-        {/* NOTE : instead of for loop to iterate over resList. use map(), filter(), reduce() */}
-        {listOfRestaurants.map((restaurant) => (
-          // <RestaurantCard/> is just a function and it is returing some jsx code. Pretty Simple and understandable.
+        {filterRestaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
