4d1ffb42b5afa5c4098f104ac7375457cb6e95fb
poorvaditya behre <aditya.behre@gmail.com>
2024-03-03 13:51:03 +0530

    added Outlet and Link to perform client side routing

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
 import About from "./components/About.js";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Contact from "./components/Contact.js";
 import Error from "./components/Error.js";
 // App Layout
 const AppLayout = () => {
   return (
     <div className="app">
+      {/* header will be intact. */}
       <Header />
-      <Body />
+      {/* here , outlet will render the childrens that will be passed to the parent. 
+        This Outlet will be replaced by the component according to the path. Hence it wont be visible inside html we you see developers console.*/}
+      <Outlet />
     </div>
   );
 };
@@ -20,9 +23,26 @@ const AppLayout = () => {
 // means if path "/" then load AppLayout component
 // Now we need to provide this to render it . For this
 const appRouter = createBrowserRouter([
-  { path: "/", element: <AppLayout />, errorElement: <Error /> },
-  { path: "/about", element: <About /> },
-  { path: "/contact", element: <Contact /> },
+  {
+    // parent route or root route
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- day-1/src/components/Header.js
+++ day-1/src/components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 // Header component
 const Header = () => {
   // header will have logo and navItem
@@ -15,9 +16,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"
