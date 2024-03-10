00e347f47a6d51d6ed7943f28b82faf3a7bdb945
poorvaditya behre <aditya.behre@gmail.com>
2024-03-10 12:19:50 +0530

    added componentDidMount() and how react lifecycle works

--- day-1/src/components/About.js
+++ day-1/src/components/About.js
@@ -1,15 +1,42 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent constructor");
+  }
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is Food App</h2>
-      <User />
-      <UserClass name={"Poorvaditya Class"} location={"Pune"} />
-    </div>
-  );
-};
+  componentDidMount() {
+    //constructor is called -> then render() -> then after component is mounted -> then componentDidMount will be called.
+    // so it is best suited for API Calls
+    console.log("Parent component did mount.");
+  }
+
+  render() {
+    console.log("Parent render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is Food App</h2>
+        {/* class based component  */}
+        <UserClass name={"Poorvaditya Class"} location={"Pune"} />
+        <UserClass name={"Aniket Class"} location={"Kharagpur"} />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//       <h2>This is Food App</h2>
+//       {/* <User /> */}
+//       {/* class based component  */}
+//       <UserClass name={"Poorvaditya Class"} location={"Pune"} />
+//     </div>
+//   );
+// };
 
 export default About;
--- day-1/src/components/UserClass.js
+++ day-1/src/components/UserClass.js
@@ -5,26 +5,43 @@ class UserClass extends React.Component {
   // for accessing props
   constructor(props) {
     super(props);
-    console.log(props);
-
     // creating state variables inside class based component
     this.state = {
       count: 0,
-      count2: 2,
     };
+
+    console.log(this.props.name, "child constructor");
+  }
+
+  componentDidMount() {
+    //constructor is called -> then render() -> then after component is mounted -> then componentDidMount will be called.
+    // so it is best suited for API Calls
+    console.log(this.props.name, "Child component did mount.");
   }
 
   //you need to provide render method which return some piece of jsx
   render() {
+    console.log(this.props.name, "child render");
     // destructuring
     const { name, location } = this.props;
 
-    const { count, count2 } = this.state;
+    const { count } = this.state;
 
     return (
       <div className="user-card">
         <h1>Count : {count}</h1>
-        <h1>Count2: {count2}</h1>
+        <button
+          onClick={() => {
+            // Never update component directy -> this.state.count = this.state.count + 1
+            // update state variable
+            // react will re-render the component and update the component
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count Increase
+        </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @pb.wtf18</h4>
