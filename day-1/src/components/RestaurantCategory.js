fdc8b23bffbec2759ece0f93d042bfb77ff069a1
poorvaditya behre <aditya.behre@gmail.com>
2024-03-15 10:09:13 +0530

    added acordian in RestaurantCategory

--- day-1/src/components/RestaurantCategory.js
+++ day-1/src/components/RestaurantCategory.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
 const RestaurantCategory = ({ data }) => {
-  console.log(data);
+  //   console.log(data);
+  const [showItems, setShowItems] = useState(false);
+
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
+
   return (
     <div>
       {/* Header */}
-      <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-6 flex justify-between">
-        <span className="font-bold text-lg">
-          {data.title} ({data.itemCards.length})
-        </span>
-        <span>⬇</span>
+      <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-6 ">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="font-bold text-lg">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span>⬇</span>
+        </div>
+        {/* Accordian Body */}
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
-      {/* Accordian Body */}
     </div>
   );
 };
