2aa692fe4fd1a8f09fb20db482ba20284faa8073
poorvaditya behre <aditya.behre@gmail.com>
2024-04-14 17:05:32 +0530

    fixed swiggy api parsing issue

--- day-1/src/components/RestaurantMenu.js
+++ day-1/src/components/RestaurantMenu.js
@@ -8,19 +8,21 @@ import RestaurantCategory from "./RestaurantCategory.js";
 const RestaurantMenu = () => {
   const { restaurantId } = useParams();
   const resInfo = useRestaurantMenu(restaurantId);
-  const [showIndex, setShowIndex] = useState(null); 
+  const [showIndex, setShowIndex] = useState(null);
+
+  console.log(resInfo);
   if (resInfo === null) {
     return <Shimmer />;
   }
 
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
 
-  const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card;
+  // const { itemCards } =
+  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card;
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
